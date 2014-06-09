d7dea0e4d141397cd02a72d525729015bf8fc9a2
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-09 05:19:23 -0700

    remove cruft

--- slackConfig14.1ROOT.sh
+++ slackConfig14.1ROOT.sh
@@ -39,9 +39,6 @@ fi
 wget -N $BASHRC -P ~/
 wget -N $BASHPR -P ~/
 
-
-#mkdir -p ~/.vim/colors/
-#wget -N https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/c.vim -P ~/.vim/colors
 wget -N $VIMRC -P ~/
 
 ## configure lilo
