9b2808bf8cff30cd33193cfaf2b87fab230d460b
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-11 20:52:11 -0700

    add input options for mate and vanilla ...
    instead of having to specify variables inline

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -39,6 +39,30 @@ EOF
 fi
 
 
+read -r -p "Would you like to go vanilla? [y/N]: " response
+case $response in
+  [yY][eE][sS]|[yY])
+    export VANILLA=true;
+    echo You are going vanilla.;
+    ;;
+  *)
+    echo You are not going vanilla.;
+    ;;
+esac
+
+
+read -r -p "Would you like to install Mate? [y/N]: " response
+case $response in
+  [yY][eE][sS]|[yY])
+    export MATE=true;
+    echo You have chosen to install Mate.;
+    ;;
+  *)
+    echo You are not installing Mate.;
+    ;;
+esac
+
+
 ## configure lilo
 sed -i 's/^#compact/lba32\
 compact/g' /etc/lilo.conf
