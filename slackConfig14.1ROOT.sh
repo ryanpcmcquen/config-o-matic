8375de6280a86c2128c289d201214546eea68294
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-11 21:09:40 -0700

    quoting ...

--- slackConfig14.1ROOT.sh
+++ slackConfig14.1ROOT.sh
@@ -35,8 +35,8 @@ EOF
   exit 1
 fi
 
-read -r -p "Would you like to go vanilla? [y/N]: " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+read -r -p "Would you like to go vanilla? [y/N]: " "response"
+if [[ "$response" =~ "^([yY][eE][sS]|[yY])$" ]]; then
     export VANILLA=true;
     echo You are going vanilla.;
 else
