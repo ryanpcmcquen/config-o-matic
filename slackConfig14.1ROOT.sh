0e399c58579d3171744a39f7a1450cb20e29845c
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-11 21:11:05 -0700

    more quoting

--- slackConfig14.1ROOT.sh
+++ slackConfig14.1ROOT.sh
@@ -38,9 +38,9 @@ fi
 read -r -p "Would you like to go vanilla? [y/N]: " "response"
 if [[ "$response" =~ "^([yY][eE][sS]|[yY])$" ]]; then
     export VANILLA=true;
-    echo You are going vanilla.;
+    echo "You are going vanilla.";
 else
-    echo You are not going vanilla.;
+    echo "You are not going vanilla.";
 fi
 
 #read -r -p "Would you like to install Mate? [y/N]: " response
