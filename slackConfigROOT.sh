2656c1e9b29e0916ca88aa824df9be4dbacd4ab7
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-28 15:18:26 -0700

    add possible superkey-launch at startup

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -218,6 +218,9 @@ else
   
   if [ -z "$( ls /var/log/packages/ | grep superkey-launch )" ]; then
     sbopkg -B -i superkey-launch
+    ## untested
+    #echo "superkey-launch" > /etc/rc.d/rc.superkey-launch
+    #chmod +x /etc/rc.d/rc.superkey-launch
   fi
   
   if [ -z "$( ls /var/log/packages/ | grep lxterminal )" ]; then
