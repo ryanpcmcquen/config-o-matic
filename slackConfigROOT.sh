2f97bbb3b6eee687d6064e11e2625f5f3b8b5cd0
Ryan P.C. McQuen <ryan.q@linux.com>
2014-08-13 19:55:03 -0700

    fix broken vi in tmux

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -140,12 +140,13 @@ sed -i \
 wget -N $BASHRC -P ~/
 wget -N $BASHPR -P ~/
 
-
 wget -N $VIMRC -P ~/
 
 
 ## set tmux scrollback value
 tmux set-option -g history-limit 9999
+## set to xterm otherwise vi will break
+tmux set-option -g default-terminal xterm-color
 
 
 ## git config
