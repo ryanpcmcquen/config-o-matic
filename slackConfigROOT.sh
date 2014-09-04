cf06ca6d7f22a26ec0b24e8cb7dce87e80a921c3
Ryan P.C. McQuen <ryan.q@linux.com>
2014-09-04 07:47:22 -0700

    remove SDL_Pango

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -413,9 +413,10 @@ elif [ "$MISCELLANY" = true ]; then
     sbopkg -B -i SDL_sound
   fi
 
-  if [ -z "$( ls /var/log/packages/ | grep SDL_Pango )" ]; then
-    sbopkg -B -i SDL_Pango
-  fi
+  ## doesn't compile on current
+  #if [ -z "$( ls /var/log/packages/ | grep SDL_Pango )" ]; then
+  #  sbopkg -B -i SDL_Pango
+  #fi
 
   if [ -z "$( ls /var/log/packages/ | grep dwm )" ]; then
     sbopkg -B -i dwm
