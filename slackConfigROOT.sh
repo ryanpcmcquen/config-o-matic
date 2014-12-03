b3fa16147798d68fae3b99d4307415d0b24f52e8
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-03 08:06:31 -0800

    version bump

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -8,7 +8,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=5.9.9
+CONFIGOMATICVERSION=5.9.10
 
 
 if [ ! $UID = 0 ]; then
