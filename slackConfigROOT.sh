f7a88ea9b56fa8529f986714125f7235b9d8fc9a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-30 15:17:17 -0800

    forgot to bump version

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -8,7 +8,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=6.7.13
+CONFIGOMATICVERSION=6.7.14
 
 
 if [ ! $UID = 0 ]; then
