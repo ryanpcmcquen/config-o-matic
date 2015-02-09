1ce3c263d6a5146b3df340a3a42f4646f5043063
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-08 19:52:36 -0800

    $ change

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -8,7 +8,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=6.7.23
+CONFIGOMATICVERSION=6.7.24
 
 
 if [ ! $UID = 0 ]; then
