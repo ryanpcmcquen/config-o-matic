d1f3e2d4646d6bec87ca1c24b80165189cd701d0
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-17 09:39:19 -0700

    add json-c, $ change

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -8,7 +8,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=7.2.21
+CONFIGOMATICVERSION=7.2.22
 
 
 if [ ! $UID = 0 ]; then
@@ -825,6 +825,9 @@ if [ "$SPPLUSISINSTALLED" = true ] && [ "$SBOPKGISINSTALLED" = true ]; then
 
     ## requires pysetuptools
     no_prompt_sbo_pkg_install_or_upgrade speedtest-cli
+
+    ## useful to have
+    no_prompt_sbo_pkg_install_or_upgrade json-c
 
     ## hydrogen
     no_prompt_sbo_pkg_install_or_upgrade scons
