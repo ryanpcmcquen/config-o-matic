60e5d06e666183f3af21c027aefb7300cc04033a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-25 10:29:18 -0700

    don't need file-roller and xarchiver

--- slackConfigROOT.sh
+++ slackConfigROOT.sh
@@ -8,7 +8,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=7.2.25
+CONFIGOMATICVERSION=7.2.26
 
 
 if [ ! $UID = 0 ]; then
@@ -926,7 +926,7 @@ if [ "$SPPLUSISINSTALLED" = true ] && [ "$SBOPKGISINSTALLED" = true ]; then
     no_prompt_sbo_pkg_install_or_upgrade libisofs
     no_prompt_sbo_pkg_install_or_upgrade libburn
     no_prompt_sbo_pkg_install_or_upgrade libisoburn
-    no_prompt_sbo_pkg_install_or_upgrade file-roller
+    #no_prompt_sbo_pkg_install_or_upgrade file-roller
     no_prompt_sbo_pkg_install_or_upgrade isomaster
     no_prompt_sbo_pkg_install_or_upgrade xarchiver
     no_prompt_sbo_pkg_install_or_upgrade thunar-archive-plugin
