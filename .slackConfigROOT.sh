6f5b6f55935172f4f7121898877ac5cfc8a2b09a
Ryan P.C. McQuen <ryan.q@linux.com>
2016-02-09 16:42:57 -0800

    add asound.conf back

--- .slackConfigROOT.sh
+++ .slackConfigROOT.sh
@@ -6,7 +6,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=7.9.03
+CONFIGOMATICVERSION=7.9.04
 
 
 if [ ! $UID = 0 ]; then
@@ -677,6 +677,10 @@ if [ -z "$(grep wifir /etc/rc.d/rc.local)" ]; then
   echo "fi" >> /etc/rc.d/rc.local
   echo >> /etc/rc.d/rc.local
 fi
+
+## fix pulseaudio issues (especially microphone operation)
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/asound.conf \
+  -P /etc/
 
 ## a script to allow promptless saving of xfce
 ## screenshots, with a nice timestamp
