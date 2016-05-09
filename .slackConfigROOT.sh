1308140141d88541e7e66468aaed0f5cedab78bd
Ryan P.C. McQuen <ryan.q@linux.com>
2016-05-09 08:45:29 -0700

    Add Haskell support to Atom.

--- .slackConfigROOT.sh
+++ .slackConfigROOT.sh
@@ -6,7 +6,7 @@
 ## note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files
-CONFIGOMATICVERSION=8.1.00
+CONFIGOMATICVERSION=8.1.01
 
 
 if [ ! $UID = 0 ]; then
