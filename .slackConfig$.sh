e6eeb73a3ab4618ac8097da7f024df2f5b39581d
Ryan P.C. McQuen <ryan.q@linux.com>
2016-08-08 19:19:03 -0700

    Remove linters and bring in Nuclide.

--- .slackConfig$.sh
+++ .slackConfig$.sh
@@ -183,7 +183,7 @@ wget -N $SCITECONF \
   -P ~/
 
 ## atom goodies
-[ `which atom` ] && apm install atom-beautify linter linter-jshint atom-ternjs language-diff language-haskell atomic-chrome emmet-simplified \
+[ `which atom` ] && apm install atom-beautify nuclide atom-ternjs language-diff language-haskell atomic-chrome emmet-simplified \
   && wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.jsbeautifyrc -P ~/
 
 echo
