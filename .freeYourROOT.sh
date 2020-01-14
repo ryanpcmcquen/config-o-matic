05259f8a788d96abdd701619fefedb3009576a89
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-01-13 17:55:20 -0800

    9.3.03

--- .freeYourROOT.sh
+++ .freeYourROOT.sh
@@ -6,7 +6,7 @@
 ## Note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files.
-CONFIGOMATICVERSION=9.3.02
+CONFIGOMATICVERSION=9.3.03
 
 
 if [ ! $UID = 0 ]; then
