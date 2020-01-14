c111ef51a83ddc534bceb670f00125cea8f92ef2
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-01-14 15:20:52 -0800

    10.0.09

--- .slackConfigROOT.sh
+++ .slackConfigROOT.sh
@@ -6,7 +6,7 @@
 ## Note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files.
-CONFIGOMATICVERSION=10.0.08
+CONFIGOMATICVERSION=10.0.09
 
 
 if [ ! $UID = 0 ]; then
