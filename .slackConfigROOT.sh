a8687205b1814732cad48304b06eae7d22fdf85d
Ryan McQuen <rpcm@linux.com>
2022-02-05 19:46:13 -0800

    10.0.16

--- .slackConfigROOT.sh
+++ .slackConfigROOT.sh
@@ -6,7 +6,7 @@
 ## Note that some configuration options may not match
 ## depending on the system, as config-o-matic tries
 ## to avoid overwriting most files.
-CONFIGOMATICVERSION=10.0.15
+CONFIGOMATICVERSION=10.0.16
 
 
 if [ ! $UID = 0 ]; then
